4426e040b5047a8c5254eef377ce8be8dc2d440b
Di Zhu <zhudi0907@hotmail.com>
2016-04-27 20:20:40 -0400

    Add bowling pins and collision detection

---
+++ Assets/Scripts/PlayerCollisionControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class PlayerCollisionControl : MonoBehaviour {
+	private Rigidbody rb;
+	private int count;
+
+	public Text scoreText;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+		rb = GetComponent<Rigidbody> ();
+		scoreText = "Score" + count.ToString ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("BowlingPins")) {
+			other.gameObject.SetActive (false);
+			count += 10;
+			scoreText = "Score" + count.ToString ();
+		}
+	}
+}
