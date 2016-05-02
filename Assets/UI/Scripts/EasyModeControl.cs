fe2c54a64f90752636ce2f481ff9ab166eb812b8
dailinshen <dailinshen@gmail.com>
2016-05-01 22:17:03 -0400

    update linda

--- Assets/UI/Scripts/EasyModeControl.cs
+++ Assets/UI/Scripts/EasyModeControl.cs
@@ -4,12 +4,21 @@ using UnityEngine.UI;
 
 public class EasyModeControl : MonoBehaviour {
 	public Canvas ExitWindow;
+
+	public Canvas EditorWindow;
+	public bool backtogame;
+
 	public bool exitpressed;
 	private GUIStyle guistyle=new GUIStyle();
 	private Text counterText;
 	private float seconds, minutes;
 	public GameObject Player;
 	public Vector3 StartPosition;
+
+	public void Backtogame(){
+		EditorWindow.enabled = false;
+		backtogame = true;
+	}
 
 	public void Exit(){
 		ExitWindow.enabled = true;
@@ -30,6 +39,10 @@ public class EasyModeControl : MonoBehaviour {
 	void Start () {
 		ExitWindow = ExitWindow.GetComponent<Canvas> ();
 		ExitWindow.enabled = false;
+
+		EditorWindow.enabled = true;
+		backtogame = false;
+
 		exitpressed = false;
 		counterText = GetComponent<Text> () as Text;
 		Player = GameObject.Find ("Player");
@@ -39,14 +52,21 @@ public class EasyModeControl : MonoBehaviour {
 	void OnGUI(){
 		minutes = (int)(Time.timeSinceLevelLoad / 60f);
 		seconds = (int)(Time.timeSinceLevelLoad % 60f);
-		guistyle.fontSize = 30;
+		guistyle.fontSize = 50;
+
 		guistyle.normal.textColor = Color.green;
 		//guistyle.fontStyle=
-		GUI.Label (new Rect (50, 20, 400, 50), minutes.ToString ("00") + ":" + seconds.ToString ("00"),guistyle);
+		GUI.Label (new Rect (500, 15, 400, 50), minutes.ToString ("00") + ":" + seconds.ToString ("00"),guistyle);
 	}
 	// Update is called once per frame
 	void Update () {
+		bool flag;
+		flag = GameObject.Find ("EditorWorkspace").GetComponent<Pathmove> ().playflag;
 		if (exitpressed)
+			Time.timeScale = 0;
+		else if (!flag)
+			Time.timeScale = 0;
+		else if (!backtogame)
 			Time.timeScale = 0;
 		else
 			Time.timeScale= 1;
